3adc16ddd3371a41d294773dbe66f655f42eb986
nepp.plus <nepp.plus@gamil.com>
2022-05-06 10:23:15 +0900

    Adapter를 통해서 실제 데이터를 대입

--- app/src/main/java/com/neppplus/myjickbangcopy_20220506/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/myjickbangcopy_20220506/adapters/RoomAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.myjickbangcopy_20220506.R
 import com.neppplus.myjickbangcopy_20220506.models.RoomData
+import java.text.DecimalFormat
 
 // 1. 어댑터 생성후 ArrayAdapter "상속" 받은 후, ArrayAdapter가 필요한 인수를 넘겨준다
 class RoomAdapter(
@@ -20,10 +22,39 @@ class RoomAdapter(
         var tempRow = convertView
 //    4. if 조건문을 통해서 한줄을 보여줄 View가 절대로 Null이 되지 않도록 작업
         if (tempRow == null) {
-            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+            tempRow = LayoutInflater.from(mContext).inflate(resId, null)
         }
-//    5. 작업이 완료된(Not-Null) 한줄을 실제로 보내줄 row라는 변수에 대입, row변수 return
+//    5. 작업이 완료된(Not-Null) 한줄을 실제로 보내줄 row라는 변수에 대입
         val row = tempRow!!
+
+//    6. 실제 row에 roomData를 가공하여 return으로 출력
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+        val data = mList[position]
+        val decimal = DecimalFormat("#,###")
+
+    val resultPrice = if (data.price > 10000) {
+        val uk = data.price / 10000
+        val remain = data.price % 10000
+        "${uk}억$remain"
+    } else {
+        decimal.format(data.price)
+    }
+
+    val resultLevel = if (data.level > 0) {
+        "${data.level}층"
+    } else if (data.level == 0) {
+        "반지층"
+    } else {
+        "지하 ${data.level * -1}층"
+    }
+
+        priceTxt.text = resultPrice
+        addressTxt.text = "${data.address}, $resultLevel"
+        descriptionTxt.text = data.description
+
         return row
     }
 }
