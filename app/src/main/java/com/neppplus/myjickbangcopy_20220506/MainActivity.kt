5390c64d9f7819977bf52599bf57bbf2e4904dc4
nepp.plus <nepp.plus@gamil.com>
2022-05-06 10:04:24 +0900

    RoomAdapter를 Activity에서 객체화

--- app/src/main/java/com/neppplus/myjickbangcopy_20220506/MainActivity.kt
+++ app/src/main/java/com/neppplus/myjickbangcopy_20220506/MainActivity.kt
@@ -2,11 +2,16 @@ package com.neppplus.myjickbangcopy_20220506
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.myjickbangcopy_20220506.adapters.RoomAdapter
 import com.neppplus.myjickbangcopy_20220506.models.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //    1. 리스트뷰에 표시할 ArrayList 만들기
     val mRoomList = ArrayList<RoomData>()
+
+//    3. 어댑터를 멤버 변수화
+    lateinit var mRoomAdapter : RoomAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,5 +28,9 @@ class MainActivity : AppCompatActivity() {
         mRoomList.add(RoomData(35000, "마포구 연남동", -1, "지하 1층 상가 임대"))
         mRoomList.add(RoomData(8500, "마포구 연남동", 4, "풀옵션 넓은 원룸"))
         mRoomList.add(RoomData(10000, "마포구 연남동", 3, "3층 빌라 임대"))
+
+//        4. 만들어 놓은 어댑터를 액티비티에서 객체화
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mRoomAdapter
     }
 }
